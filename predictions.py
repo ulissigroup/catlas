fcc16fc615ef45b22f0a56f16a8c55896f022e01
Brook Wander <brookwander@github.com>
2021-09-28 19:17:00 +0000

    pushing Pourbaix changes

--- predictions.py
+++ predictions.py
@@ -13,6 +13,9 @@ from calculator_upload_empty import (
     relaxation_energy_prediction,
 )
 
+from pymatgen.analysis.pourbaix_diagram import PourbaixDiagram
+from pymatgen.ext.matproj import MPRester
+
 from ocpmodels.models.dimenet_plus_plus import DimeNetPlusPlusWrap
 import os
 from enumerate_surfaces import enumerate_surfaces
@@ -33,26 +36,63 @@ if __name__ == "__main__":
 with open(inputs_path) as f:
     inputs = yaml.safe_load(f)
 
+## general
 adsorbate_smiles = inputs["adsorbate_smile_list"]
 num_workers = inputs["num_workers"]
+
+output_options = inputs["output_options"]
+
+## filters
 size_filter = inputs["filter_by_object_size"]
+max_size = size_filter["max_size"]
+
 mpid_filter = inputs["filter_by_mpids"]
+mpids_to_include = mpid_filter["mpid_list"]
+
 element_filter = inputs["filter_by_elements"]
+elements_to_include = element_filter["element_list"]
+
 num_elements_filter = inputs["filter_by_number_elements"]
+num_elements = num_elements_filter["number_of_els"]
+
+pourbaix_filter = inputs['filter_by_Pourbaix_stability']
+pH_allowed = pourbaix_filter['pH']
+pH_step = pourbaix_filter['pH_step']
+pH_range = np.arange(pH_allowed[0], (float(pH_allowed[1]) + float(pH_step)), pH_step)
+AP_allowed = pourbaix_filter['applied_potential']
+AP_step = pourbaix_filter['applied_potential_step']
+AP_range = np.arange(AP_allowed[0], (float(AP_allowed[1]) + float(AP_step)), AP_step)
+decomp_max = pourbaix_filter['max_decomposition_E']
+
+## paths
 checkpoint_path = inputs["checkpoint_path"]
 config_path = inputs["config_path"]
 worker_spec_path = inputs["worker_spec_path"]
-num_elements = num_elements_filter["number_of_els"]
-elements_to_include = element_filter["element_list"]
-mpids_to_include = mpid_filter["mpid_list"]
-max_size = size_filter["max_size"]
-output_options = inputs["output_options"]
+
+
+
+
+
+
+
+with open('/home/jovyan/ocp/dask-predictions/mpid_to_most_recent_mpid.pkl', 'rb') as f:
+    old_mpid_to_new = pickle.load(f)
 
 
 def load_bulk_pkl():
     with open(BULK_PKL, "rb") as f:
         bulks = pickle.load(f)
         return bulks[1] + bulks[2] + bulks[3]
+    
+def pourbaix_grabber(mpid):
+    with MPRester('MGOdX3P4nI18eKvE') as mpr:
+        composition = mpr.get_entries(mpid)[0].composition
+    comp_dict = {str(key): value for key, value in composition.items()
+                         if str(key) not in ['H','O']}
+    entries = mpr.get_pourbaix_entries(list(comp_dict.keys()))
+    entry = [entry for entry in entries if entry.entry_id == mpid][0]
+    pbx = PourbaixDiagram(entries, comp_dict=comp_dict, filter_solids=False)
+    return pbx, entry
 
 
 def load_all_ads():
@@ -110,6 +150,21 @@ def filter_surfaces_by_size(surface, max_size=max_size):
     surface_object, mpid, millers, shift, top = surface
     return len(surface_object.surface_atoms) <= max_size
 
+def filter_bulk_by_pourbaix(bulk, old_mpid_to_new = old_mpid_to_new, pH_range = pH_range,
+                            AP_range = AP_range, decomp_max = decomp_max):
+    atoms, mpid = bulk
+    pbx_get = pourbaix_memory.cache(pourbaix_grabber)
+    mpid_new = old_mpid_to_new[mpid]
+    pbx, entry = pbx_get(mpid_new)
+    for pH in pH_range:
+        for AP in AP_range:
+            decomp_energy = pbx.get_decomposition_energy(entry, pH, AP)
+            if decomp_energy <= decomp_max:
+                return True
+    return False
+            
+    
+
 
 def run_filter_bulks_enumerate_adslabs():
 
@@ -129,6 +184,9 @@ def run_filter_bulks_enumerate_adslabs():
 
     if mpid_filter["enable"]:
         filtered_bulk_bag = filtered_bulk_bag.filter(filter_bulk_by_mpids)
+        
+    if pourbaix_filter['enable']:
+        filtered_bulk_bag = filtered_bulk_bag.filter(filter_bulk_by_pourbaix)
 
     # Repartition
     filtered_bulk_bag = bag_split_individual_partitions(filtered_bulk_bag)
@@ -171,7 +229,7 @@ def run_predictions(enumerated_adslabs, mode):
 
 cache_location = inputs["shared_scratch_path"] + "/cachedir"
 memory = Memory(cache_location, verbose=1)
-
+pourbaix_memory = Memory('/home/jovyan/shared-scratch/Inference/pourbaix_cachedir', verbose=1)
 
 cluster = KubeCluster(worker_spec_path, deploy_mode="local")
 cluster.adapt(minimum=1, maximum=num_workers, interval="30000 ms")
