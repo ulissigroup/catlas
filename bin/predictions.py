d85a6733c749e11462ff4bade9609f776ddfbaf2
Kirby Broderick <kirbybroderick@gmail.com>
2021-10-01 18:25:11 +0000

    adsorbate filtering

--- bin/predictions.py
+++ bin/predictions.py
@@ -1,6 +1,6 @@
 import yaml
 from dask_predictions.load_bulk_structures import load_ocdata_bulks
-from dask_predictions.filters import bulk_filter
+from dask_predictions.filters import bulk_filter, adsorbate_filter
 from dask_predictions.load_adsorbate_structures import load_ocdata_adsorbates
 import dask.bag as db
 import dask
@@ -20,11 +20,12 @@ if __name__ == "__main__":
     filtered_bulk_dataframe = bulk_filter(config, bulk_dataframe)
     filtered_bulk_dataframe = filtered_bulk_dataframe.compute()
 
-    print('Total number of bulks is %d' %filtered_bulk_dataframe.shape[0])
+    print("Total number of bulks is %d" % filtered_bulk_dataframe.shape[0])
 
     adsorbate_delayed = dask.delayed(load_ocdata_adsorbates)()
     adsorbate_bag = db.from_delayed([adsorbate_delayed])
     adsorbate_dataframe = adsorbate_bag.to_dataframe()
     adsorbate_dataframe = adsorbate_dataframe.compute()
+    filtered_adsorbate_dataframe = adsorbate_filter(config, adsorbate_dataframe)
 
-    print('Total number of adsorbates is %d' %adsorbate_dataframe.shape[0])
+    print("Total number of adsorbates is %d" % adsorbate_dataframe.shape[0])
--- dask_predictions/filters.py
+++ dask_predictions/filters.py
@@ -1,46 +1,37 @@
 import warnings
 import numpy as np
 
-all_bulk_filters = [
-    'mpids',
-    'elements',
-    'number_elements',
-    'object_size',
-    'Pourbaix_stability'
-]
 
-all_surf_filters = [
-    
-]
-
-def filterparse_config(config):
-   
-    filters = {name:val for name,val in config.items() if (len(name) >= 10) and (name[:9] == 'filter_by') and (config[name]['enable'])}
-    
-    bulk_filters = {name:val for name,val in filters.items() if name[10:] in all_bulk_filters}
-    surf_filters = {name:val for name,val in filters.items() if name[10:] in all_surf_filters}
-    unknown_filters = {name:val for name,val in filters.items() if (name not in bulk_filters) and (name not in surf_filters)}
-    
-    if len(unknown_filters)>0:
-        warnings.warn('Filters could not be parsed: ' + str([c for c in unknown_filters.keys()]))
-    
-    return bulk_filters, surf_filters
-    
-        
 def bulk_filter(config, dask_df):
-    bulk_filters, _ = filterparse_config(config)
-    
+    bulk_filters = config["bulk_filters"]
+
     for name, val in bulk_filters.items():
-        if name == 'filter_by_mpids':
-            dask_df = dask_df[dask_df.bulk_mpid.isin(config['filter_by_mpids']['mpid_list'])]
-        elif name == 'filter_by_Pourbaix_stability':
-            pass
-        elif name == 'filter_by_elements':
-            dask_df = dask_df[dask_df.bulk_elements.apply(lambda x: all([el in config['filter_by_elements']['element_list'] for el in x]))]
-        elif name == 'filter_by_num_elements':
-            dask_df = dask_df[dask_df.bulk_elements.apply(len).isin(config['filter_by_number_elements']['number_of_els'])]
-        elif name == 'filter_by_object_size':
-            dask_df = dask_df[dask_df.bulk_natoms <= config['filter_by_object_size']['max_size']]
-        else:
-            warnings.warn('Filter is not implemented: ' + name)
+        if val != "None":
+            if name == "filter_by_mpids":
+                dask_df = dask_df[dask_df.bulk_mpid.isin(val)]
+            elif name == "filter_by_Pourbaix_stability":
+                pass
+            elif name == "filter_by_elements":
+                dask_df = dask_df[
+                    dask_df.bulk_elements.apply(lambda x: all([el in val for el in x]))
+                ]
+            elif name == "filter_by_num_elements":
+                dask_df = dask_df[dask_df.bulk_elements.apply(len).isin(val)]
+            elif name == "filter_by_object_size":
+                dask_df = dask_df[dask_df.bulk_natoms <= val]
+            else:
+                warnings.warn("Filter is not implemented: " + name)
+    return dask_df
+
+
+def adsorbate_filter(config, dask_df):
+    adsorbate_filters = config["adsorbate_filters"]
+
+    for name, val in adsorbate_filters.items():
+        if val != "None":
+            if name == "filter_by_smiles":
+                dask_df = dask_df[dask_df.adsorbate_smiles.isin(val)]
+            else:
+                warnings.warn("Filter is not implemented: " + name)
+
     return dask_df
