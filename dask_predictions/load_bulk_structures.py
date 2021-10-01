e5065bb6de977ec510d2be492ccea6143b8791ed
zack <zulissi@gmail.com>
2021-10-01 14:49:19 +0000

    reorg and stripped down predictions.py as a test

--- dask_predictions/load_bulk_structures.py
+++ dask_predictions/load_bulk_structures.py
@@ -1,13 +1,16 @@
 import dask.bag as db
 import dask
 from ase.db import connect
+import os.path
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 def load_ocdata_bulks():
-    with connect('../bulk_structures/ocdata_bulks.db') as db:
+    with connect(dir_path+'/bulk_structures/ocdata_bulks.db') as db:
 
         # Turn each entry into a dictionary that will become the dataframe columns
         bulk_list = []
-        for row in tqdm.tqdm(db.select()):
+        for row in db.select():
             bulk_list.append({'bulk.atoms': row.toatoms(),
                               'bulk.mpid': row.mpid,
                               'bulk.data_source': 'ocdata_bulks',
