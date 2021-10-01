6f7610286148f8cb679161b8653ac2250a0f1b6a
zack <zulissi@gmail.com>
2021-10-01 19:13:56 +0000

    add computes

--- bin/predictions.py
+++ bin/predictions.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     bulk_dataframe = bulk_bag.to_dataframe()
     filtered_bulk_dataframe = bulk_filter(config, bulk_dataframe)
     filtered_bulk_dataframe = filtered_bulk_dataframe.persist()
-    print("Total number of filtered bulks is %d" % filtered_bulk_dataframe.shape[0])
+    print("Total number of filtered bulks is %d" % filtered_bulk_dataframe.compute().shape[0])
 
     # Load and filter the adsorbates
     adsorbate_delayed = dask.delayed(load_ocdata_adsorbates)()
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     filtered_adsorbate_dataframe = adsorbate_filter(config, adsorbate_dataframe)
     print(
         "Total number of filtered adsorbates is %d"
-        % filtered_adsorbate_dataframe.shape[0]
+        % filtered_adsorbate_dataframe.shape[0].compute()
     )
