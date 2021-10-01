a659f9d2765317bb6dfec2ddf50c9c9c6978e879
zack <zulissi@gmail.com>
2021-10-01 16:34:04 +0000

    add adsorbate loader

---
+++ dask_predictions/load_adsorbates.py
@@ -0,0 +1,17 @@
+import pickle
+import os.path
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def load_ocdata_adsorbates():
+    with open(dir_path+'/adsorbates/adsorbates.pkl','rb') as fhandle:
+        adsorbates = pickle.load(fhandle)
+
+        adsorbate_list = []
+        for atoms, smiles, bond_indices in adsorbates:
+            adsorbate_list.append({'adsorbate.atoms': atoms,
+                                   'adsorbate.smiles': smiles,
+                                   'adsorbate.bond_indices': bond_indices,
+                                   'adsorbate.data_source': 'ocdata_adsorbates'})
+
+        return adsorbate_list
