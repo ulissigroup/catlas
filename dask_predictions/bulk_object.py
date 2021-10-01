2e01f9e99e1a3506ef0aeb1e4b285d7fda1a0f36
Brook Wander <brookwander@github.com>
2021-10-01 18:22:28 +0000

    tricked by the new black test :)

--- dask_predictions/bulk_object.py
+++ dask_predictions/bulk_object.py
@@ -1,8 +1,8 @@
 from ocdata.bulk_obj import Bulk
 
+
 class CustomBulk(Bulk):
     def __init__(self, bulk):
         bulk_atoms, mpid = bulk
         self.bulk_atoms = bulk_atoms
         self.mpid = mpid
-    
