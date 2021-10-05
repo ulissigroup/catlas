e2145083cb05b604b495d6d90f982fc7fddf72e4
Kirby Broderick <kirbybroderick@gmail.com>
2021-10-05 18:13:55 +0000

    setup.py reworked

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name="dask-predictions",
+    name="CATlas",
     version="0.0",
     description="Python Distribution Utilities",
-    author="Brook Wander, Kirby Broderick, and others in the ulissi research group",
+    author="Brook Wander, Kirby Broderick, and others in the Ulissi research group",
     author_email="bwander@andrew.cmu.edu",
-    url="https://github.com/ulissigroup/dask-predictions",
-    packages=["dask_predictions"],
+    url="https://github.com/ulissigroup/CATlas",
+    packages=["CATlas"],
 )
