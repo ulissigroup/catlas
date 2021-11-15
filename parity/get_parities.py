5122594f7a6a4e4ce3bc03d68b606af4308f7e40
Brook Wander <brook.l.wander@gmail.com>
2021-11-15 16:18:30 +0000

    reformating with black and fixing ood ads bug

--- parity/get_parities.py
+++ parity/get_parities.py
@@ -1,4 +1,10 @@
-from parity_utils import get_predicted_E, data_preprocessing, apply_filters, get_specific_smile_plot, get_general_plot
+from parity_utils import (
+    get_predicted_E,
+    data_preprocessing,
+    apply_filters,
+    get_specific_smile_plot,
+    get_general_plot,
+)
 import yaml
 import sys
 
@@ -8,18 +14,16 @@ if __name__ == "__main__":
     config_path = sys.argv[1]
     with open(config_path) as f:
         config = yaml.safe_load(f)
-        
-    
+
     # Load the data
-    df = data_preprocessing(config['npz_file_path'], config['dft_data_path'])
-    
+    df = data_preprocessing(config["npz_file_path"], config["dft_data_path"])
+
     # Apply filters
     df_filtered = apply_filters(config, df)
-    
+
     # Generate adsorbate specific plots
-    for smile in config['desired_adsorbate_smiles']:
-        get_specific_smile_plot(smile, df_filtered, config['npz_file_path'])
-        
+    for smile in config["desired_adsorbate_smiles"]:
+        get_specific_smile_plot(smile, df_filtered, config["npz_file_path"])
+
     # Generate overall model plot
-    get_general_plot(df_filtered, config['npz_file_path'])
-   
+    get_general_plot(df_filtered, config["npz_file_path"])
