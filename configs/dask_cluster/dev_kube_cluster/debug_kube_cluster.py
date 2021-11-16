c6b9a56db0e2c264e5030aedf4834d5cce146b06
Kirby Broderick <kirbybroderick@gmail.com>
2021-11-16 02:53:10 +0000

    fixed errors with debug_kube_cluster

--- configs/dask_cluster/dev_kube_cluster/debug_kube_cluster.py
+++ configs/dask_cluster/dev_kube_cluster/debug_kube_cluster.py
@@ -22,8 +22,8 @@ cluster = KubeCluster(
     name="dask-catlas-dev",
     scheduler_service_wait_timeout=120,
 )
-cluster.scale(10)
+# cluster.scale(10)
 
-kube_cluster_new_worker(cluster, "configs/debug_configs/workers-cpu-dev.yml")
-cluster.scale(60)
+# kube_cluster_new_worker(cluster, "configs/debug_configs/workers-cpu-dev.yml")
+# cluster.scale(60)
 client = Client(cluster)
