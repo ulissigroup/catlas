a33acc8ad5f2847817ea35ee3d564a9c55e99131
Zack Ulissi <zulissi@gmail.com>
2021-11-18 16:43:44 +0000

    typo in config for github

--- configs/dask_cluster/github_kube_cluster/github_kube_cpu_cluster.py
+++ configs/dask_cluster/github_kube_cluster/github_kube_cpu_cluster.py
@@ -26,7 +26,7 @@ cluster = KubeCluster(
     pod_template="configs/dask_cluster/github_kube_cluster/worker-cpu-github.yml",
     scheduler_pod_template=scheduler_pod_template,
     namespace="zulissi",
-    name = "dask-catlas-{{ GITHUB_RUN_ID }}-{{ GITHUB_JOB }}"
+    name="dask-catlas-{{ GITHUB_RUN_ID }}-{{ GITHUB_JOB }}",
     scheduler_service_wait_timeout=480,
 )
 cluster.scale(2)
