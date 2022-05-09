86078fa933638a050e2bd886cc3ae32c3b1aae9a
Brook Wander <brook.l.wander@gmail.com>
2022-05-09 15:00:44 +0000

    adding packages that dont sit in the top level dir

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
     from distutils.core import setup
 
 setup(
     name="catlas",
     version="0.0",
-    description="Python Distribution Utilities",
+    description="Python Distribution Utilities for enumerating adslabs and performing ML inference of energies",
     author="Brook Wander, Kirby Broderick, and others in the Ulissi research group",
     author_email="bwander@andrew.cmu.edu",
     url="https://github.com/ulissigroup/catlas",
-    packages=["catlas"],
+    packages= find_packages(),
 )
