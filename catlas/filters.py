427569adcb5eaf2119b7c000a833937829ceec5f
Brook Wander <brook.l.wander@gmail.com>
2022-07-20 13:48:52 +0000

    making filters generalize for use in parity filtering

--- catlas/filters.py
+++ catlas/filters.py
@@ -2,12 +2,11 @@ import warnings
 import numpy as np
 from pymatgen.core.periodic_table import Element
 from catlas.filter_utils import get_pourbaix_stability, get_elements_in_groups
-from catlas.sankey.sankey_utils import Sankey
 import catlas.cache_utils
 import catlas.dask_utils
 
 
-def bulk_filter(config, dask_df, sankey, initial_bulks):
+def bulk_filter(config, dask_df, sankey=None, initial_bulks=None):
     """
     Filters a dask dataframe `dask_df` of bulk structures according to rules specified in a config yml `config`.
 
@@ -22,14 +21,17 @@ def bulk_filter(config, dask_df, sankey, initial_bulks):
         sankey: the sankey object with added info
     """
     bulk_filters = config["bulk_filters"]
-    sankey_idx = 2
-    sankey.info_dict["label"][1] = f"Bulks from db ({initial_bulks})"
+    columns = dask_df.columns.compute()
+    
+    if sankey is not None:
+        sankey_idx = 2
+        sankey.info_dict["label"][1] = f"Bulks from db ({initial_bulks})"
 
     for name, val in bulk_filters.items():
         if (
             str(val) != "None"
         ):  # depending on how yaml is specified, val may either be "None" or NoneType
-            if name == "filter_by_bulk_ids":
+            if name == "filter_by_bulk_ids" and "bulk_id" in columns:
                 dask_df = dask_df[dask_df.bulk_id.isin(val)]
             elif name == "filter_ignore_bulk_ids":
                 dask_df = dask_df[~dask_df.bulk_id.isin(val)]
@@ -58,7 +60,7 @@ def bulk_filter(config, dask_df, sankey, initial_bulks):
                         required_elements=val,
                     )
                 ]
-            elif name == "filter_by_object_size":
+            elif name == "filter_by_object_size" and "bulk_natoms" in columns:
                 dask_df = dask_df[dask_df.bulk_natoms <= val]
             elif name == "filter_by_elements_active_host":
                 dask_df = dask_df[
@@ -98,10 +100,10 @@ def bulk_filter(config, dask_df, sankey, initial_bulks):
                     )
                 ]
 
-            elif name == "filter_by_bulk_e_above_hull":
+            elif name == "filter_by_bulk_e_above_hull" and "bulk_e_above_hull" in columns:
                 dask_df = dask_df[dask_df.bulk_e_above_hull <= val]
 
-            elif name == "filter_by_bulk_band_gap":
+            elif name == "filter_by_bulk_band_gap" and "bulk_band_gap" in columns:
                 if "min_gap" in val and "max_gap" in val:
                     dask_df = dask_df[
                         (dask_df.bulk_band_gap >= val["min_gap"])
@@ -126,36 +128,40 @@ def bulk_filter(config, dask_df, sankey, initial_bulks):
                     + '" filtered bulk df down to length '
                     + str(len(dask_df))
                 )
-            # Update the sankey dictionary
-            node_loss = initial_bulks - len(dask_df)
-            initial_bulks = len(dask_df)
-            sankey.update_dictionary(
-                f"Rejected by {name} ({node_loss})",
-                1,
-                sankey_idx,
-                node_loss,
-                1,
-                "tbd",
-            )
-            sankey_idx += 1
-    # Add remaining bulks to the sankey and connect them to slabs
-    sankey.update_dictionary(
-        f"Filtered bulks ({initial_bulks})",
-        1,
-        sankey_idx,
-        initial_bulks,
-        0.2,
-        0.5,
-    )
-    sankey.update_dictionary(
-        "Slabs",
-        sankey_idx,
-        sankey_idx + 1,
-        initial_bulks,
-        0.4,
-        0.5,
-    )
-    return dask_df, sankey
+            if sankey is not None:
+                # Update the sankey dictionary
+                node_loss = initial_bulks - len(dask_df)
+                initial_bulks = len(dask_df)
+                sankey.update_dictionary(
+                    f"Rejected by {name} ({node_loss})",
+                    1,
+                    sankey_idx,
+                    node_loss,
+                    1,
+                    "tbd",
+                )
+                sankey_idx += 1
+    if sankey is not None:
+        # Add remaining bulks to the sankey and connect them to slabs
+        sankey.update_dictionary(
+            f"Filtered bulks ({initial_bulks})",
+            1,
+            sankey_idx,
+            initial_bulks,
+            0.2,
+            0.5,
+        )
+        sankey.update_dictionary(
+            "Slabs",
+            sankey_idx,
+            sankey_idx + 1,
+            initial_bulks,
+            0.4,
+            0.5,
+        )
+        return dask_df, sankey
+    else:
+        return dask_df
 
 
 def slab_filter(config, dask_dict):
