0fc378146e6e488e80075ffb617b68984f6d1342
Kirby Broderick <kirbybroderick@gmail.com>
2022-08-15 20:08:41 +0000

    improved formatting of configs

--- configs/dask_cluster/github_kube_cluster/github_kube_gpu_cluster.py
+++ configs/dask_cluster/github_kube_cluster/github_kube_gpu_cluster.py
@@ -26,7 +26,7 @@ cluster = KubeCluster(
     pod_template="configs/dask_cluster/github_kube_cluster/worker-gpu-github.yml",
     scheduler_pod_template=scheduler_pod_template,
     namespace="zulissi",
-    name="dask-catlas-%s" % os.environ["GITHUB_RUN_ID"],
+    name=f"""dask-catlas-{os.environ["GITHUB_RUN_ID"]}""",
     scheduler_service_wait_timeout=480,
 )
 cluster.scale(1)
