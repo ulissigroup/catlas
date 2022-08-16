c2aba00de66914a150350a61d7effc1e4f1a9649
Kirby Broderick <kirbybroderick@gmail.com>
2022-08-16 17:32:50 +0000

    docstring

--- bin/process_npz_for_validation.py
+++ bin/process_npz_for_validation.py
@@ -1,9 +1,13 @@
-"""Create validation pickle files from npz predictions on val."""
-
 from catlas.parity.data_processing_utils import ProcessValNPZ
 import argparse
 
 if __name__ == "__main__":
+    """Create validation pickle files from npz predictions on val.
+
+    Args:
+        npz_path (str): File path where model output npz files are found.
+        dft_df_path (str): File path where DFT data is found.
+    """
     # Parse arguments:
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--npz_path", type=str)
